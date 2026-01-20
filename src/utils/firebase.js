75db338c82460604bce01fa0b17a94b3aadc05d7
slinke-hub <privatepple@gmail.com>
2026-01-20 20:50:19 +0300

    save

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyDk3mnCJcQoP3wCZ5ubi6Owvw6bZoSZ2_I",
+    authDomain: "gpa-class-reports.firebaseapp.com",
+    projectId: "gpa-class-reports",
+    storageBucket: "gpa-class-reports.appspot.com",
+    messagingSenderId: "932091328883",
+    appId: "y1:932091328883:web:d9fcf107d70638cc73ad06"
 };
 
 const app = initializeApp(firebaseConfig);
